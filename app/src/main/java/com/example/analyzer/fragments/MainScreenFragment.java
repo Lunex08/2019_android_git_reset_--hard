35e771dbbd81b69f5f369bc7b753b3b0f765ec3a
Lunex.08 <Lunex.08@gmail.com>
2019-11-13 01:18:00 +0300

    feature(main_fragment): add settings button into toolbar, set blue theme

--- app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
+++ app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
@@ -2,8 +2,12 @@ package com.example.analyzer.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +26,23 @@ public class MainScreenFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.main_screen_fragment, container, false);
+
+        setHasOptionsMenu(true);
+
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.settings_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Settings screen will be soon...",
+                Toast.LENGTH_SHORT);
+        toast.show();
+        return super.onOptionsItemSelected(item);
     }
 }
