0c01d6b6a66ecb4e8a3be93da00a0d209f3dacfb
mrazhev <mrazhev@aquivalabs.com>
2019-11-19 23:18:16 +0300

    bugfix(MainScreenFragment): display toolbar menu item

--- app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
+++ app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
@@ -1,16 +1,14 @@
 package com.example.analyzer.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
@@ -18,6 +16,8 @@ import androidx.fragment.app.Fragment;
 import com.example.analyzer.R;
 
 public class MainScreenFragment extends Fragment implements View.OnClickListener {
+    final static public String TAG = "MainScreenFragmentTag";
+
     public static MainScreenFragment getInstance() {
         return new MainScreenFragment();
     }
@@ -29,19 +29,25 @@ public class MainScreenFragment extends Fragment implements View.OnClickListener
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.main_screen_fragment, container, false);
 
-        Toolbar toolbar = v.findViewById(R.id.toolbar);
-        toolbar.setTitle("MyAnalyzer");
+        final Toolbar toolbar = v.findViewById(R.id.toolbar);
+        toolbar.setTitle("Analyzer");
         toolbar.getMenu().clear();
         toolbar.inflateMenu(R.menu.settings_menu);
-//        toolbar.setOnMenuItemClickListener(a->{ return true;});
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        final Menu menu = toolbar.getMenu();
+        menu.findItem(R.id.settings_menu).setOnMenuItemClickListener((menuItem) -> {
+            Log.d(TAG, "Toolbar menu clicked");
+            return true;
+        });
+
+
         final CardView cardView = v.findViewById(R.id.card_view);
         cardView.setOnClickListener(this);
 
         return v;
     }
-  
+
     public void onClick(View v) {
-        // View your tariff fragment
+        Log.d(TAG, "Tarif cardView clicked");
     }
 }
