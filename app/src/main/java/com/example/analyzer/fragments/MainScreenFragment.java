a2b01563fb4dd3f38058a6f23f4f2be93fa822b3
mrazhev <mrazhev@aquivalabs.com>
2019-11-19 10:54:59 +0300

    doesnt show toolbar menu

--- app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
+++ app/src/main/java/com/example/analyzer/fragments/MainScreenFragment.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.analyzer.R;
@@ -27,22 +27,19 @@ public class MainScreenFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.main_screen_fragment, container, false);
 
-        setHasOptionsMenu(true);
+        Toolbar toolbar = v.findViewById(R.id.toolbar);
+        toolbar.setTitle("MyAnalyzer");
+        toolbar.getMenu().clear();
+        toolbar.inflateMenu(R.menu.settings_menu);
+//        toolbar.setOnMenuItemClickListener(a->{ return true;});
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
 
         return v;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.settings_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Settings screen will be soon...",
-                Toast.LENGTH_SHORT);
-        toast.show();
-        return super.onOptionsItemSelected(item);
+        ((AppCompatActivity) getActivity()).getMenuInflater().inflate(R.menu.settings_menu, menu);
     }
 }
