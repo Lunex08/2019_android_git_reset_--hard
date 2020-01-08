5be8c77d93b83b0a8d0ea843056a406131c2dd51
Mikhail Razhev <mrazhev@aquivalabs.com>
2020-01-08 19:06:53 +0300

    bugfix(PermissionsUtils): request multiple permissions at once

--- app/src/main/java/com/example/analyzer/utils/PermissionsUtils.java
+++ app/src/main/java/com/example/analyzer/utils/PermissionsUtils.java
@@ -8,24 +8,27 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermissionsUtils {
     public static final String TAG = "PermissionsUtilsTag";
     private static int requestCode = 0;
     private static final String REQUIRED_PERMISSION_MESSAGE = "This app required permission to run: ";
 
     public static boolean checkAndRequestPermissions(@NonNull Activity activity, String[] permissions) {
+        List<String> remainPermissions = new ArrayList<>();
         for (String permission : permissions) {
             int permissionCheck = ContextCompat.checkSelfPermission(activity.getApplicationContext(), permission);
-
             if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
                     Toast.makeText(activity, REQUIRED_PERMISSION_MESSAGE + permission, Toast.LENGTH_SHORT).show();
                 }
-                ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode++);
-                return false;
+                remainPermissions.add(permission);
             }
         }
 
+        ActivityCompat.requestPermissions(activity, remainPermissions.toArray(new String[0]), requestCode++);
         return true;
     }
 }
